24d7ec49292f8e01015eb38880e646e7892b0334
HIROE Shin <hiroe.orz@gmail.com>
2010-08-14 22:25:42 +0900

    created Song and Artist view

---
+++ Classes/ArtistFriendsViewController.m
@@ -0,0 +1,35 @@
+//
+//  ArtistFriendsViewController.m
+//  NowPlayingFriends
+//
+//  Created by Hiroe Shin on 10/08/14.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "ArtistFriendsViewController.h"
+#import "TwitterClient.h"
+
+
+@implementation ArtistFriendsViewController
+
+- (void)refreshTimeline {
+
+  NSLog(@"updating timeline data...");
+
+  TwitterClient *client = [[TwitterClient alloc] init];
+  NSString *songTitle = [self.appDelegate nowPlayingTitle];
+  NSString *artistName = [self.appDelegate nowPlayingArtistName];
+
+  NSArray *newTimeline = [client getSearchTimeLine:@"#nowplaying", 
+				 artistName, nil];
+
+  @synchronized(timeline) {
+    self.timeline = newTimeline;
+  }
+
+  [client release];
+
+  NSLog(@"timeline data updated.");
+}
+
+@end
---
+++ Classes/SongFriendsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SongFriendsViewController.h
+//  NowPlayingFriends
+//
+//  Created by Hiroe Shin on 10/08/14.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FriendsViewController.h"
+
+
+@interface SongFriendsViewController : FriendsViewController {
+
+}
+
+@end
